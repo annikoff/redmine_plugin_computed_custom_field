0e91246429f2d5c774a212a2db64b3389f48e5b2
y.annikov <y.annikov@demis.ru>
2015-08-13 23:50:22 +0300

    Change list of classes for patch. Add output format for CF

--- lib/computizator.rb
+++ lib/computizator.rb
@@ -1,9 +1,7 @@
 module ComputedCustomFieldPlugin
   class Computizator
     def self.patch_for_computing_cfs
-      classes = ActiveRecord::Base.descendants
-          .select {|i| i.reflect_on_all_associations(:has_many)
-                           .detect{ |r| r.name == :custom_values }}
+      classes = [Issue, Project, User]
       classes.each do |klass|
         unless klass.included_modules.include?(ComputedCustomFieldPlugin::KlassPatch)
           klass.send(:include, ComputedCustomFieldPlugin::KlassPatch)
--- lib/patches/klass_patch.rb
+++ lib/patches/klass_patch.rb
@@ -10,11 +10,14 @@ module ComputedCustomFieldPlugin
       custom_field_values.each do |value|
         next unless value.custom_field.computed?
         formula = value.custom_field.formula
+        output_format = value.custom_field.output_format.first
         cf_ids = formula.scan(/%\{cf_(\d+)\}/).flatten
+        #FIX IT
         cf_ids.each do |cf_id|
-          formula.sub!("%{cf_#{cf_id}}", custom_field_value(cf_id))
+          formula.sub!("%{cf_#{cf_id.to_s}}",
+                       custom_field_value(cf_id).try("to_#{output_format}"))
         end
-        value.value = eval(formula)
+        self.custom_field_values = {value.custom_field.id => eval(formula)}
       end
     end
   end
