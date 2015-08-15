9a9a839617cae7cfa061538c9e4e5cc74d8d6de9
y.annikov <y.annikov@demis.ru>
2015-08-15 13:26:57 +0300

    Conversion error fix

--- lib/patches/klass_patch.rb
+++ lib/patches/klass_patch.rb
@@ -11,11 +11,10 @@ module ComputedCustomFieldPlugin
         next unless value.custom_field.computed?
         formula = value.custom_field.formula
         output_format = value.custom_field.output_format.first
-        cf_ids = formula.scan(/%\{cf_(\d+)\}/).flatten
-        #FIX IT
+        cf_ids = formula.scan(/%\{cf_(\d+)\}/).flatten.map(&:to_i)
         cf_ids.each do |cf_id|
-          formula.sub!("%{cf_#{cf_id.to_s}}",
-                       custom_field_value(cf_id).try("to_#{output_format}"))
+          formula.sub!("%{cf_#{cf_id}}",
+                       custom_field_value(cf_id).try("to_#{output_format}").to_s)
         end
         self.custom_field_values = {value.custom_field.id => eval(formula)}
       end
