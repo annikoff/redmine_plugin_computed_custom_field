90a75f5a34c747c34c2c31b924e9a419fd382759
annikoff <annikoff@ya.ru>
2017-01-24 21:41:51 +0300

    remove format view, add avaliable cfs list, update .travis.yml

--- lib/computed_custom_field/custom_field_patch.rb
+++ lib/computed_custom_field/custom_field_patch.rb
@@ -5,6 +5,7 @@ module ComputedCustomField
     included do
       before_save -> { self.editable = false; true }, if: :is_computed?
       before_save -> { self.formula ||= '' }, if: :is_computed?
+      before_save -> { self.is_computed = true }, if: :is_computed?
       validates_with FormulaValidator, if: :is_computed?
     end
   end
