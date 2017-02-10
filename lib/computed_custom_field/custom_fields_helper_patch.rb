36a9b76522c75270d7acf337a6cc8523cdf71a07
annikoff <y.annikov@demis.ru>
2017-02-10 22:30:44 +0300

    fix error

--- lib/computed_custom_field/custom_fields_helper_patch.rb
+++ lib/computed_custom_field/custom_fields_helper_patch.rb
@@ -1,8 +1,7 @@
 module ComputedCustomField
   module CustomFieldsHelperPatch
     def render_computed_custom_fields_select(custom_field)
-      cfs = CustomField.where(:type => custom_field.type)
-                       .where('custom_fields.id != ?', custom_field.id || 0)
+      cfs = CustomField.where(:type => custom_field.type).where('custom_fields.id != ?', custom_field.id || 0)
 
       select_tag '',
                  options_from_collection_for_select(cfs, 'id', 'name'),
