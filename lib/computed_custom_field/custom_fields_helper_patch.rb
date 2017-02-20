723b68978db5441fa77e1669a7144212058b51af
annikoff <annikoff@ya.ru>
2017-02-20 18:22:44 +0300

    additional information

--- lib/computed_custom_field/custom_fields_helper_patch.rb
+++ lib/computed_custom_field/custom_fields_helper_patch.rb
@@ -1,12 +1,14 @@
 module ComputedCustomField
   module CustomFieldsHelperPatch
     def render_computed_custom_fields_select(custom_field)
-      cfs = CustomField.where(type: custom_field.type)
+      fields = CustomField.where(type: custom_field.type)
                        .where('custom_fields.id != ?', custom_field.id || 0)
+      options = fields.map do |field|
+        is_computed = field.is_computed? ? ", #{l(:field_is_computed)}" : ''
+        ["#{field.name} (#{field.field_format}#{is_computed})", field.id]
+      end
 
-      select_tag '',
-                 options_from_collection_for_select(cfs, 'id', 'name'),
-                 size: 5, multiple: true, id: 'available_cfs'
+      select_tag '', options_for_select(options), size: 5, multiple: true, id: 'available_cfs'
     end
   end
 end
