26ddca37c20d92220d8eaef71a99f42e6cc43181
Anatoly Lapkov <a.lapkov@gmail.com>
2019-01-15 01:33:52 +0700

    Update init.rb

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :computed_custom_field do
   author 'Yakov Annikov'
   url 'https://github.com/annikoff/redmine_plugin_computed_custom_field'
   description ''
-  version '1.0.6'
+  version '1.0.7'
   settings default: {}
 end
 
